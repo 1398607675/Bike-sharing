62e22d93f49f7ebd2bad6354c1f915fcc23b3afb
没啥说的劈个叉吧 <1398607675@qq.com>
2021-04-30 14:17:12 +0800

    修改菜单

--- src/components/NavLeft/index.jsx
+++ src/components/NavLeft/index.jsx
@@ -1,39 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Menu } from 'antd';
 import MenuConfig from './../../resource/menuConfig';
 import './index.less';
 
 const SubMenu = Menu.SubMenu;
-export default class NavLeft extends Component {
-  componentWillMount(){
-    const menuTreeNode = this.renderMenu(MenuConfig);
-    this.setState({
-      menuTreeNode
-    })
-  }
-  renderMenu = (data) => {
-    return data.map((item)=>{
-      if(item.children){
-        return (
-          <SubMenu title={item.title} key={item.key}>
-            {this.renderMenu(item.children)}
-          </SubMenu>
-        )
+const NavLeft = () => {
+  const renderMenu = (data) => {
+    return data.map((item) => {
+      if (item.children) {
+        return <SubMenu title={item.title} key={item.key}>
+          {renderMenu(item.children)}
+        </SubMenu>
       }
       return <Menu.Item title={item.title} key={item.key}>{item.title}</Menu.Item>
     })
   }
-  render() {
-    return (
-      <div>
-        <div className='logo'>
-          <img src="/assets/logo-ant.svg" alt="" />
-          <h1>IMOOS MS</h1>
-        </div>
-        <Menu theme='dark'>
-          {this.state.menuTreeNode}
-        </Menu>
+  const MenuTreeNode = renderMenu(MenuConfig);
+  return (
+    <div>
+      <div className='logo'>
+        <img src="/assets/logo-ant.svg" alt="" />
+        <h1>Imooc MS</h1>
       </div>
-    )
-  }
+      <Menu theme='dark'>
+        {MenuTreeNode}
+      </Menu>
+    </div>
+  );
 }
+
+export default NavLeft;
